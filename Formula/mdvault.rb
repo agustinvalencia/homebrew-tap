c59e607f3c95dda40e6b7b4d253b4e9391815895
agustin valencia <aguvalenciag@gmail.com>
2026-02-19 14:55:26 +0100

    mdvault 0.3.2

--- Formula/mdvault.rb
+++ Formula/mdvault.rb
@@ -1,28 +1,28 @@
 class Mdvault < Formula
   desc "CLI tool for managing markdown vaults with structured notes and validation"
   homepage "https://github.com/agustinvalencia/mdvault"
-  version "0.3.0"
+  version "0.3.2"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/agustinvalencia/mdvault/releases/download/v0.3.0/mdv-0.3.0-x86_64-apple-darwin.tar.gz"
-      sha256 "dde3ca22838821776d95a412449a02b30c67dbc299ef88043a9504a0a580a28e"
+      url "https://github.com/agustinvalencia/mdvault/releases/download/v0.3.2/mdv-0.3.2-x86_64-apple-darwin.tar.gz"
+      sha256 "5fdde49cdf1ddb8fdfe1e5bec0efac08ae13ce6dbcecdbbd603a5a614a95acce"
     end
     on_arm do
-      url "https://github.com/agustinvalencia/mdvault/releases/download/v0.3.0/mdv-0.3.0-aarch64-apple-darwin.tar.gz"
-      sha256 "7614387c515b199992aed8c0ded11b44661baf9f7e5d6709ed35485e051393a0"
+      url "https://github.com/agustinvalencia/mdvault/releases/download/v0.3.2/mdv-0.3.2-aarch64-apple-darwin.tar.gz"
+      sha256 "32e1fc85039e42e696cf2df9ff4db4321e0fd4275be73cbc6aca3c2d8a7387e3"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/agustinvalencia/mdvault/releases/download/v0.3.0/mdv-0.3.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "bcecbdd56b6c25bae99f7f33d5185cce40b2f40dfbe04fb780eb2a71e3144554"
+      url "https://github.com/agustinvalencia/mdvault/releases/download/v0.3.2/mdv-0.3.2-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "357d483abc8c83d1202d3f6d487220952a21335ab0445e668957a719f8499b77"
     end
     on_arm do
-      url "https://github.com/agustinvalencia/mdvault/releases/download/v0.3.0/mdv-0.3.0-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "cf42bad1d22c5a6f425ba057c685046387ab8f0b5ec56111cc9e08e512f70ab0"
+      url "https://github.com/agustinvalencia/mdvault/releases/download/v0.3.2/mdv-0.3.2-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "fe98a8dad7d28b0e889d200a9cab0f3f15bb3619b8d9dd0d4b4b1222056a581c"
     end
   end
 
