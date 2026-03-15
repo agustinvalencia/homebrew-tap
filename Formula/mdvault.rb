debdb340f4f342d2c69b650206bff2f571b19f5b
agustin valencia <aguvalenciag@gmail.com>
2026-03-15 19:26:45 +0100

    chore: bump mdvault to 0.5.1
    Bug fixes: weekly note date resolution (MDV-034).

--- Formula/mdvault.rb
+++ Formula/mdvault.rb
@@ -1,28 +1,28 @@
 class Mdvault < Formula
   desc "CLI tool for managing markdown vaults with structured notes and validation"
   homepage "https://github.com/agustinvalencia/mdvault"
-  version "0.5.0"
+  version "0.5.1"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/agustinvalencia/mdvault/releases/download/v0.5.0/mdv-0.5.0-x86_64-apple-darwin.tar.gz"
-      sha256 "830cbc7bd9dc0916a881efbbfdce0ee1206a4cbbd586bc3d7eb800c489990a17"
+      url "https://github.com/agustinvalencia/mdvault/releases/download/v0.5.1/mdv-0.5.1-x86_64-apple-darwin.tar.gz"
+      sha256 "ceda381b6166e971ee364a376c7561da8777629f3116c1d437ec54729341c730"
     end
     on_arm do
-      url "https://github.com/agustinvalencia/mdvault/releases/download/v0.5.0/mdv-0.5.0-aarch64-apple-darwin.tar.gz"
-      sha256 "d7bcbcc56709385e71e398493b985a450f7bddb833f49d85e4b77482cdd9a092"
+      url "https://github.com/agustinvalencia/mdvault/releases/download/v0.5.1/mdv-0.5.1-aarch64-apple-darwin.tar.gz"
+      sha256 "502d4961cf7cb78976e2b7487ee69991e1359945b5b2e775a9dd0b1509714580"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/agustinvalencia/mdvault/releases/download/v0.5.0/mdv-0.5.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "afd008078c8e4ee8e12352e68b478bb938263b2ace4fa44da82b45ebffb935a7"
+      url "https://github.com/agustinvalencia/mdvault/releases/download/v0.5.1/mdv-0.5.1-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "e46811d7263edaddd3c4a03d3d447f7913be72eb9f651fd61734881ce76e3642"
     end
     on_arm do
-      url "https://github.com/agustinvalencia/mdvault/releases/download/v0.5.0/mdv-0.5.0-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "e55bddcc52314be1b4894d2becf1c97df7164665c2b109188cc69fcbbfe5c9d2"
+      url "https://github.com/agustinvalencia/mdvault/releases/download/v0.5.1/mdv-0.5.1-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "0858dc388be3602966ddc7b1d048c60615700e44107f755e9699b76d9e4e64fe"
     end
   end
 
