98d0993702c1d4e169a1afc9925d663af3e14e92
agustin valencia <aguvalenciag@gmail.com>
2026-06-07 18:41:57 +0200

    cuaderno 0.1.3: bump url + sha256 for all four targets

--- Formula/cuaderno.rb
+++ Formula/cuaderno.rb
@@ -1,28 +1,28 @@
 class Cuaderno < Formula
   desc "Markdown vault manager for the Research Logbook Method (CLI + MCP server)"
   homepage "https://github.com/agustinvalencia/cuaderno"
-  version "0.1.2"
+  version "0.1.3"
   license "MPL-2.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/agustinvalencia/cuaderno/releases/download/v0.1.2/cuaderno-0.1.2-aarch64-apple-darwin.tar.gz"
-      sha256 "2673627d8bab61d335600d4ec7354a503fd38c6a25e52a24efab6ae602a9af37"
+      url "https://github.com/agustinvalencia/cuaderno/releases/download/v0.1.3/cuaderno-0.1.3-aarch64-apple-darwin.tar.gz"
+      sha256 "843774b1c26b131afc61503a4e2703b2f15de57d81a8dd81c0572ab8d75869aa"
     end
     on_intel do
-      url "https://github.com/agustinvalencia/cuaderno/releases/download/v0.1.2/cuaderno-0.1.2-x86_64-apple-darwin.tar.gz"
-      sha256 "5360ba00a51ab3e4fb15b0b2a0ee8fee09587260148c2325c16a5885243ff5fd"
+      url "https://github.com/agustinvalencia/cuaderno/releases/download/v0.1.3/cuaderno-0.1.3-x86_64-apple-darwin.tar.gz"
+      sha256 "4a1247f72896c6ef8f6f5ee24f6df2039b706acef5e1b5c8a06d6ef7957048f7"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/agustinvalencia/cuaderno/releases/download/v0.1.2/cuaderno-0.1.2-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "c8277c97620c8d3cdb070ec7b0b8eb28a36631ffff042af77ccd660bf6f98156"
+      url "https://github.com/agustinvalencia/cuaderno/releases/download/v0.1.3/cuaderno-0.1.3-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "52e7a51c8b017147ff2ebbff63a8a7d2850969036f6de8957654c9725eb5b6de"
     end
     on_intel do
-      url "https://github.com/agustinvalencia/cuaderno/releases/download/v0.1.2/cuaderno-0.1.2-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "d96a8259143b48d996d094d59d478562651151f92322b61262fa2bbd6618a9d6"
+      url "https://github.com/agustinvalencia/cuaderno/releases/download/v0.1.3/cuaderno-0.1.3-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "97a328d28238c272ec22a8db5f2b5277d5a305b798b3e0fc5c37f404719700bf"
     end
   end
 
