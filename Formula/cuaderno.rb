6e8be09e4ef013a14ce782a4df397e8684b3aa42
agustin valencia <aguvalenciag@gmail.com>
2026-06-11 16:48:48 +0200

    cuaderno 0.1.6

--- Formula/cuaderno.rb
+++ Formula/cuaderno.rb
@@ -1,28 +1,28 @@
 class Cuaderno < Formula
   desc "Markdown vault manager for the Research Logbook Method (CLI + MCP server)"
   homepage "https://github.com/agustinvalencia/cuaderno"
-  version "0.1.5"
+  version "0.1.6"
   license "MPL-2.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/agustinvalencia/cuaderno/releases/download/v0.1.5/cuaderno-0.1.5-aarch64-apple-darwin.tar.gz"
-      sha256 "f45ee6206918c2fcc4ce236f901145444851938952b52944148d4beb4484daa7"
+      url "https://github.com/agustinvalencia/cuaderno/releases/download/v0.1.6/cuaderno-0.1.6-aarch64-apple-darwin.tar.gz"
+      sha256 "21869ee4a89ecd1cdd3da1367b58cecc23a4123f1a411ad6775d65d78d69f22a"
     end
     on_intel do
-      url "https://github.com/agustinvalencia/cuaderno/releases/download/v0.1.5/cuaderno-0.1.5-x86_64-apple-darwin.tar.gz"
-      sha256 "1e33bf1d4964d1fb55fcef3c577a881a93ed5ccff75f326f4d4dac8d9113b665"
+      url "https://github.com/agustinvalencia/cuaderno/releases/download/v0.1.6/cuaderno-0.1.6-x86_64-apple-darwin.tar.gz"
+      sha256 "0c4755dc8ddca988dea0bb32b0ac8181525637c2d820aebb65f072d7a58a35a7"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/agustinvalencia/cuaderno/releases/download/v0.1.5/cuaderno-0.1.5-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "ecefd44bc3b68516448ebe2d9e6c9c5ec5f2417d538b84dfdc7102b3f97af9cf"
+      url "https://github.com/agustinvalencia/cuaderno/releases/download/v0.1.6/cuaderno-0.1.6-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "3bbdf07e96cb2adee1f99a2e4b2409e4d2da205facf6ca72b9da640996aab5b5"
     end
     on_intel do
-      url "https://github.com/agustinvalencia/cuaderno/releases/download/v0.1.5/cuaderno-0.1.5-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "44fc00868e36441b6c340e777f93acb7ac02779ccc76ffe149a6e218c7afc48b"
+      url "https://github.com/agustinvalencia/cuaderno/releases/download/v0.1.6/cuaderno-0.1.6-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "20648e08ca4d6f26d4727339003472f8a94fe4086a9f070ca83dfa4cb6f9d77b"
     end
   end
 
