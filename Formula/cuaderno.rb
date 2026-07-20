a5260e2f6208a76639de5f7e8504ff8651c8e924
agustin valencia <aguvalenciag@gmail.com>
2026-07-20 14:09:37 +0200

    cuaderno 0.29.2

--- Formula/cuaderno.rb
+++ Formula/cuaderno.rb
@@ -1,28 +1,28 @@
 class Cuaderno < Formula
   desc "Markdown vault manager for the Research Logbook Method (CLI + MCP server)"
   homepage "https://github.com/agustinvalencia/cuaderno"
-  version "0.29.1"
+  version "0.29.2"
   license "MPL-2.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/agustinvalencia/cuaderno/releases/download/v0.29.1/cuaderno-0.29.1-aarch64-apple-darwin.tar.gz"
-      sha256 "896ee3f97a7f3e1d32385e88cbfc7f7ca8614f00e3b40580ba4d1b91672d8f67"
+      url "https://github.com/agustinvalencia/cuaderno/releases/download/v0.29.2/cuaderno-0.29.2-aarch64-apple-darwin.tar.gz"
+      sha256 "bf0cb64b786b7d757ab399a2dce25f66c03a8d12c0df63d3dc54e83c91d1bf07"
     end
     on_intel do
-      url "https://github.com/agustinvalencia/cuaderno/releases/download/v0.29.1/cuaderno-0.29.1-x86_64-apple-darwin.tar.gz"
-      sha256 "1f07813c10ca6a0155653112da6785fa8298d5a5264670375286301f8811d69f"
+      url "https://github.com/agustinvalencia/cuaderno/releases/download/v0.29.2/cuaderno-0.29.2-x86_64-apple-darwin.tar.gz"
+      sha256 "b716b21f6f5cc1846379b25d5e16eaa5c1bb68413a5d318f361c182afab2dc53"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/agustinvalencia/cuaderno/releases/download/v0.29.1/cuaderno-0.29.1-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "6801f7c43f975bd7229af5a383866de7efe4dd5169ba7a4e14d5f94b74ceb577"
+      url "https://github.com/agustinvalencia/cuaderno/releases/download/v0.29.2/cuaderno-0.29.2-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "f66830720e3f5bb465544f693d39eb13cf82db1f0bfc914886b889b0031a53f7"
     end
     on_intel do
-      url "https://github.com/agustinvalencia/cuaderno/releases/download/v0.29.1/cuaderno-0.29.1-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "cf4005dcb85d8699b884442afc5758a18ebec4f05a38c0da200c8650595db308"
+      url "https://github.com/agustinvalencia/cuaderno/releases/download/v0.29.2/cuaderno-0.29.2-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "0e4f8bbffc6cdc299dfee958b0dad70f35d394b92e3e61e3d5b261a375064373"
     end
   end
 
